1e397aff683270faa847abb9ab9c74824aab8413
lirusi <lirusilondon@gmail.com>
2018-12-03 13:52:50 +0000

    a note about this py file

---
+++ work.py
@@ -0,0 +1,7 @@
+#this is a demo to test git functions
+print("people works")
+input=input("have you worked today?")
+if input==("yes"):
+    print("work harder")
+else:
+    print("work work, continue work")
